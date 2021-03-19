82e4d020914715a0e31ef7efe98b19908ff00b88
Manny Sandhu <manny.sandhu.1992@gmail.com>
2021-03-19 11:37:52 +0000

    init push

---
+++ my-library-backend/src/main/java/io/github/mannysandhu/MyLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.mannysandhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ my-library-backend/src/test/java/io/github/mannysandhu/MyLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mannysandhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
